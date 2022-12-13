f0af0074d34bfa8f80b96997544ae07e36aeb30e
NAYEEM <nayeem0772@gmail.com>
2022-12-13 21:24:16 +0530

    sample selenium test files

--- demo1.py
+++ demo1.py
@@ -1,10 +1,21 @@
 from selenium import webdriver
-#chrome driver
+
 from selenium.webdriver.chrome.service import Service
+# from selenium.webdriver.firefox.service import Service
+# from selenium.webdriver.edge.service import Service
 
-
+# chrome driver
 service_obj=Service("C:/Users/NAYEEM/Downloads/SEL/chromedriver.exe")
 driver=webdriver.Chrome(service=service_obj)
+
+#firefoxdriver
+# service_obj=Service("C:/Users/NAYEEM/Downloads/SEL/geckodriver.exe")
+# driver=webdriver.Firefox(service=service_obj)
+
+#Edgedriver
+# service_obj=Service("C:/Users/NAYEEM/Downloads/SEL/msedgedriver.exe")
+# driver=webdriver.Edge(service=service_obj)
+
 driver.maximize_window()
 driver.get("https://www.facebook.com")
 print(driver.title)
